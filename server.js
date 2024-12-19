4e5621fb6604143aa1a2bada3667f2fddd42c9f2
Mostafa Mohamed <newvisitor2015@proton.me>
2024-12-19 22:18:18 +0200

    Connected

--- server.js
+++ server.js
@@ -1,1 +1,164 @@
-const WebSocket = require('ws'); const crypto = require('crypto'); const http = require('http'); const server = http.createServer((req, res) => { res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Server is running'); }); const wss = new WebSocket.Server({ server }); const rooms = new Map(); function generateRoomCode() { return crypto.randomBytes(3).toString('hex'); } function getConnectedUsers(room) { return Array.from(room.clients).map(client => client.username); } wss.on('connection', (ws) => { let roomCode = null; let username = null; ws.on('message', (message) => { const data = JSON.parse(message); switch (data.type) { case 'create': roomCode = generateRoomCode(); username = data.username; ws.username = username; rooms.set(roomCode, { host: ws, clients: new Set([ws]), filename: null, state: { playing: false, time: 0 } }); ws.send(JSON.stringify({ type: 'created', roomCode })); break; case 'join': roomCode = data.roomCode; username = data.username; ws.username = username; const room = rooms.get(roomCode); if (room) { room.clients.add(ws); broadcastToRoom(roomCode, { type: 'userList', users: getConnectedUsers(room) }); ws.send(JSON.stringify({ type: 'joined', filename: room.filename, state: room.state })); } break; case 'videoSelected': if (rooms.has(roomCode)) { const room = rooms.get(roomCode); room.filename = data.filename; broadcastToRoom(roomCode, { type: 'videoUpdate', filename: data.filename }, ws); } break; case 'videoControl': if (rooms.has(roomCode)) { const room = rooms.get(roomCode); room.state = { playing: data.action === 'play', time: data.time }; broadcastToRoom(roomCode, { type: 'videoControl', action: data.action, time: data.time }, ws); } break; } }); ws.on('close', () => { if (roomCode && rooms.has(roomCode)) { const room = rooms.get(roomCode); room.clients.delete(ws); if (room.clients.size === 0) { rooms.delete(roomCode); } else if (room.host === ws) { rooms.delete(roomCode); broadcastToRoom(roomCode, { type: 'hostLeft' }); } else { broadcastToRoom(roomCode, { type: 'userList', users: getConnectedUsers(room) }); } } }); }); function broadcastToRoom(roomCode, message, exclude = null) { const room = rooms.get(roomCode); if (room) { room.clients.forEach((client) => { if (client !== exclude) { client.send(JSON.stringify(message)); } }); } } server.listen(3000, () => { console.log('Server running on http://localhost:3000'); });
+const WebSocket = require('ws');
+const crypto = require('crypto');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Server is running');
+});
+
+const wss = new WebSocket.Server({ 
+    server,
+    perMessageDeflate: {
+        zlibDeflateOptions: { level: 9 }
+    }
+});
+
+const rooms = new Map();
+
+function generateRoomCode() {
+    return crypto.randomBytes(3).toString('hex');
+}
+
+function getConnectedUsers(room) {
+    return Array.from(room.clients).map(client => client.username);
+}
+
+wss.on('connection', (ws) => {
+    let roomCode = null;
+    let username = null;
+    ws.binaryType = 'arraybuffer';
+
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        
+        switch(data.type) {
+            case 'create':
+                roomCode = generateRoomCode();
+                username = data.username;
+                ws.username = username;
+                
+                rooms.set(roomCode, {
+                    host: ws,
+                    clients: new Set([ws]),
+                    filename: null,
+                    state: {
+                        playing: false,
+                        time: 0
+                    },
+                    lastUpdate: Date.now()
+                });
+                
+                ws.send(JSON.stringify({
+                    type: 'created',
+                    roomCode: roomCode
+                }));
+                break;
+
+            case 'join':
+                roomCode = data.roomCode;
+                username = data.username;
+                ws.username = username;
+                
+                const room = rooms.get(roomCode);
+                if (room) {
+                    room.clients.add(ws);
+                    const users = getConnectedUsers(room);
+                    room.clients.forEach(client => {
+                        if (client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify({
+                                type: 'userList',
+                                users: users
+                            }));
+                        }
+                    });
+                    
+                    ws.send(JSON.stringify({
+                        type: 'joined',
+                        filename: room.filename,
+                        state: room.state
+                    }));
+                }
+                break;
+
+            case 'videoSelected':
+                if (rooms.has(roomCode)) {
+                    const videoRoom = rooms.get(roomCode);
+                    videoRoom.filename = data.filename;
+                    videoRoom.clients.forEach(client => {
+                        if (client !== ws && client.readyState === WebSocket.OPEN) {
+                            client.send(JSON.stringify({
+                                type: 'videoUpdate',
+                                filename: data.filename
+                            }));
+                        }
+                    });
+                }
+                break;
+
+            case 'videoControl':
+                if (rooms.has(roomCode)) {
+                    const controlRoom = rooms.get(roomCode);
+                    const now = Date.now();
+                    
+                    // Update room state
+                    controlRoom.state = {
+                        playing: data.action === 'play' ? true : data.action === 'pause' ? false : controlRoom.state.playing,
+                        time: Math.round(data.time * 10) / 10
+                    };
+                    
+                    // Only throttle regular updates, not play/pause actions
+                    if (data.action === 'play' || data.action === 'pause' || now - controlRoom.lastUpdate > 100) {
+                        controlRoom.lastUpdate = now;
+                        
+                        // Send the current state to all clients
+                        controlRoom.clients.forEach(client => {
+                            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                                client.send(JSON.stringify({
+                                    type: 'videoControl',
+                                    action: data.action,
+                                    time: controlRoom.state.time,
+                                    playing: controlRoom.state.playing
+                                }));
+                            }
+                        });
+                    }
+                }
+                break;
+        }
+    });
+
+    ws.on('close', () => {
+        if (roomCode && rooms.has(roomCode)) {
+            const room = rooms.get(roomCode);
+            room.clients.delete(ws);
+            
+            if (room.clients.size === 0) {
+                rooms.delete(roomCode);
+            } else if (room.host === ws) {
+                room.clients.forEach(client => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify({
+                            type: 'hostLeft'
+                        }));
+                    }
+                });
+                rooms.delete(roomCode);
+            } else {
+                const users = getConnectedUsers(room);
+                room.clients.forEach(client => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify({
+                            type: 'userList',
+                            users: users
+                        }));
+                    }
+                });
+            }
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
